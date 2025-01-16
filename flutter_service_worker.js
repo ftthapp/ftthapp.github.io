f0d7ea8c2c24ea32295bc5b5f2d8851751d3e3c7
Francis Kitema Nzoka <fkdevelopers254@gmail.com>
2025-01-16 16:47:35 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9e9866fb2f4ab55806f6a66dfe285d5a
 "assets/AssetManifest.bin.json": "9f05eb8ea68df9a29d1e2d39d245a6f4",
 "assets/AssetManifest.json": "53537682eded002004b5f423dcf1ffe6",
 "assets/FontManifest.json": "209e66e2f71c646cc7eb744ea1cea0dc",
-"assets/fonts/MaterialIcons-Regular.otf": "678b0493dbfdbde0aae3b2eaae5c8718",
+"assets/fonts/MaterialIcons-Regular.otf": "abe3ddb867d1d02ff8a21e8ef6369a6d",
 "assets/NOTICES": "56e085fb15b031b7760e56804246102e",
 "assets/packages/awesome_snackbar_content/assets/back.svg": "ba1c3aebba280f23f5509bd42dab958d",
 "assets/packages/awesome_snackbar_content/assets/bubbles.svg": "1df6817bf509ee4e615fe821bc6dabd9",
@@ -33,9 +33,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "9e9866fb2f4ab55806f6a66dfe285d5a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "37377ef360eb3d39c650e8e166d75fc9",
-"/": "37377ef360eb3d39c650e8e166d75fc9",
-"main.dart.js": "08fce7ce68f7e39815a8def60f0e1b38",
+"index.html": "295b0aa83f67ca10bf89fd48d5a56746",
+"/": "295b0aa83f67ca10bf89fd48d5a56746",
+"main.dart.js": "234f07079b2eb1d9512425f883c4bc0e",
 "manifest.json": "5790538ed70058a19c1742f1330fb4fa",
 "version.json": "e98b9a11ce7f7ec697347efefa389c29"};
 // The application shell files that are downloaded before a service worker can
